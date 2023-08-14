191102182c11b93c7571a205b5b79b903833f2dc
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-08-14 02:52:41 +0000

    6-14終了

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//監視対象が範囲内に現れたら実行する動作
+const showKirin = () => {
+  console.log('キリンさんです');
+};
+
+//監視ロボットの設定
+const kirinObserver = new IntersectionObserver(showKirin);
+
+//#kirinを監視するように指示
+kirinObserver.observe(document.querySelector('#kirin'));
